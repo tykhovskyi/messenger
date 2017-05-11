7fdd784679d590aba6d20c088622e636d5242ee0
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2017-05-11 21:17:31 +0300

    MongoDB Basics

--- routes/app.js
+++ routes/app.js
@@ -1,17 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user')
 
 router.get('/', function (req, res, next) {
-    res.render('index');
+    User.findOne({}, function (err, doc) {
+        if (err) {
+            return res.send('Error!');
+        }
+        res.render('node', {email: doc.email});
+    })
 });
 
-router.get('/message/:msg', function (req, res, next) {
-    res.render('node', {message: req.params.msg});
-});
+router.post('/', function(req, res, next) {
+    var email = req.body.email;
+    var user = new User({
+        firstName: 'John',
+        lastName: 'Doe',
+        password: 'super-secret',
+        email: email
+    })
+    user.save();
 
-router.post('/message', function(req, res, next) {
-    var message = req.body.message;
-    res.redirect('/message/' + message);
+    res.redirect('/');
 });
 
 module.exports = router;
