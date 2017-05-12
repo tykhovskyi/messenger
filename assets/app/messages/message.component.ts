8eb4b6ce2cd8e7bd329c9f5268eef034f987360a
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2017-05-12 22:10:12 +0300

    Event binding

--- assets/app/messages/message.component.ts
+++ assets/app/messages/message.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, Output, EventEmitter } from "@angular/core";
 
 import { Message } from "./message.model";
 
@@ -22,4 +22,9 @@ import { Message } from "./message.model";
 })
 export class MessageComponent {
   @Input() msg: Message;
+  @Output() editClicked = new EventEmitter<string>();
+
+  onEdit() {
+    this.editClicked.emit('A new value');
+  }
 }
