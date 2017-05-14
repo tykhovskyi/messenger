f361b5340e3e9a3f843410de0925b946ef7b5059
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2017-05-14 17:54:06 +0300

    Creating a header & re-structuring the project

---
+++ assets/app/auth/authentication.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-authentication',
+  template: '<h1>Authentication</h1>'
+})
+export class AuthenticationComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
---
+++ assets/app/messages/messages.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: `    
+    <div class="row">
+      <app-message-input></app-message-input>
+    </div>
+    <hr>
+    <div class="row">
+      <app-message-list></app-message-list>
+    </div>
+  `
+})
+export class MessagesComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
