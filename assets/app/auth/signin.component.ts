2aa9d457791919547d71faa5f8d8d7246fe6632d
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2017-05-16 12:19:19 +0300

    Finishing the Frontend

--- assets/app/auth/signin.component.ts
+++ assets/app/auth/signin.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html'
 })
 export class SigninComponent implements OnInit {
-  constructor() { }
+  myForm: FormGroup;
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.myForm = new FormGroup({
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+      ]),
+      password: new FormControl(null, Validators.required),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.myForm);
+    this.myForm.reset();
+  }
 }
--- assets/app/messages/message.component.ts
+++ assets/app/messages/message.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, Output, EventEmitter } from "@angular/core";
 
 import { Message } from "./message.model";
+import { MessageService } from "./message.service";
 
 @Component({
   selector: 'app-message',
@@ -24,7 +25,13 @@ export class MessageComponent {
   @Input() msg: Message;
   @Output() editClicked = new EventEmitter<string>();
 
+  constructor(private messageService: MessageService) {}
+
   onEdit() {
     this.editClicked.emit('A new value');
+  }
+
+  onDelete() {
+    this.messageService.deleteMessage(this.msg);
   }
 }
