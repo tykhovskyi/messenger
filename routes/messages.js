1099b30d017b3b84624f2e63dd441ccf3721d1da
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2017-05-17 22:25:41 +0300

    Backend - Updating Messages

--- routes/messages.js
+++ routes/messages.js
@@ -37,4 +37,35 @@ router.post('/', function (req, res, next) {
   });
 });
 
+router.patch('/:id', function(err, result) {
+  Message.findById(req.params.id, function (err, message) {
+    if (err) {
+      return res.status(500).json({
+        title: 'An error occurred',
+        error: err
+      });
+    }
+    if (!message) {
+      return res.status(500).json({
+        title: 'No message found!',
+        error: {message: 'Message not found'}
+      });
+    }
+
+    message.content = req.body.content;
+    message.save(function(err, result) {
+      if (err) {
+        return res.status(500).json({
+          title: 'An error occurred',
+          error: err
+        });
+      }
+      res.status(200).json({
+        message: 'Updated message',
+        obj: result
+      });
+    });
+  });
+});
+
 module.exports = router;
